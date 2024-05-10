3699081245fcb61b70b5acdbab152e8ab5d66ced
Jason Deroubaix <jason.deroubaix@gmail.com>
2024-05-10 16:05:58 +0200

    update

--- src/utils.ts
+++ src/utils.ts
@@ -222,12 +222,8 @@ export function executeIf<T>(condition: boolean, promiseFunc: () => Promise<T>):
 export const sessionIdKey = 'pokerogue_sessionId';
 export const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '';
 export const serverUrl = isLocal ? 'http://localhost:8001' : '';
-<<<<<<< HEAD
 export const apiUrl = isLocal ? serverUrl : '/api';
 export const fallbackApiUrl = isLocal ? serverUrl : 'api';
-=======
-export const apiUrl = isLocal ? serverUrl : 'https://api.pokerogue.net';
->>>>>>> 5c985291 (Re-remove fallback API change with new URL)
 
 export function setCookie(cName: string, cValue: string): void {
   const expiration = new Date();
